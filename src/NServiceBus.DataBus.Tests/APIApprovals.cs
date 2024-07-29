1f808b9764f5a268410dfff2acbfb07d2b1bb7d0
Dan Kent <dan.kent@particular.net>
2024-07-29 14:32:34 -0400

    Add approval test

---
+++ src/NServiceBus.DataBus.Tests/APIApprovals.cs
@@ -0,0 +1,19 @@
+﻿namespace NServiceBus.DataBus.Tests;
+
+using NUnit.Framework;
+using Particular.Approvals;
+using PublicApiGenerator;
+
+[TestFixture]
+public class APIApprovals
+{
+    [Test]
+    public void ApproveAzureStorageQueueTransport()
+    {
+        var publicApi = typeof(DataBus).Assembly.GeneratePublicApi(new ApiGeneratorOptions
+        {
+            ExcludeAttributes = ["System.Runtime.Versioning.TargetFrameworkAttribute", "System.Reflection.AssemblyMetadataAttribute"]
+        });
+        Approver.Verify(publicApi);
+    }
+}
---
+++ src/NServiceBus.DataBus/DataBus.cs
@@ -0,0 +1,15 @@
+﻿namespace NServiceBus.DataBus;
+
+/// <summary>
+/// DataBus class
+/// </summary>
+public class DataBus
+{
+    /// <summary>
+    /// Test API
+    /// </summary>
+    public void TestApi()
+    {
+        // Do Nothing
+    }
+}
